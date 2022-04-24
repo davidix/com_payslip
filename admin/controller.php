2b1bf4a65c6f4c076a79baf0434a609c3e20ff8c
davidix <d.bereihi@gmail.com>
2022-04-25 00:45:43 +0430

    Update controller.php

--- admin/controller.php
+++ admin/controller.php
@@ -9,37 +9,15 @@ defined("_JEXEC") or die("Restricted access");
 
 class PayslipController extends JControllerLegacy
 {
-	/**
-	 * The default view for the display method.
-	 *
-	 * @var    string
-	 * @since  12.2
-	 */
+
 	protected $default_view = 'payslips';
-	
-	/**
-	 * Checks whether a user can see this view.
-	 *
-	 * @param   string	$view	The view name.
-	 *
-	 * @return  boolean
-	 * @since   1.6
-	 */
+
 	protected function canView($view)
 	{
 		$canDo	= PayslipHelper::getActions();
 		return true;
 	}
 
-	/**
-     * Method to display a view.
-     *
-     * @param   boolean If true, the view output will be cached
-     * @param   array   An array of safe url parameters and their variable types, for valid values see {@link JFilterInput::clean()}.
-     *
-     * @return  JController	This object to support chaining.
-     * @since   1.5
-     */
     public function display($cachable = false, $urlparams = false)
 	{
         // Get the document object.
